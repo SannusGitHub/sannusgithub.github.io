9cdda5c1e1015f8f60d488a832675e7517eec28d
Sannu <111326413+SannusGitHub@users.noreply.github.com>
2025-03-01 00:22:40 +0200

    html

---
+++ js/alt.mjs
@@ -0,0 +1,57 @@
+async function login(payload) {
+    const response = await fetch("https://sannusgithub-private-vecrel.vercel.app/api/auth", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ payload })
+    });
+
+    const data = await response.json();
+    if (data.success) {
+        localStorage.setItem("token", data.token);
+        fetchData();
+    } else {
+        document.getElementById('input-box').value = "";
+    }
+}
+
+async function fetchData() {
+    const token = localStorage.getItem("token");
+
+    const response = await fetch("https://sannusgithub-private-vecrel.vercel.app/api/content", {
+        headers: { Authorization: `Bearer ${token}` }
+    });
+
+    const data = await response.json();
+    const contentDiv = document.getElementById("content");
+
+    if (response.ok) {
+        contentDiv.innerHTML = data.indexHtml;
+        document.head.insertAdjacentHTML("beforeend", `<style>${data.styleCss}</style>`);
+
+        const script = document.createElement("script");
+        script.innerHTML = data.javascriptJs + "\nsetTimeout(init, 0);";
+        document.body.appendChild(script);
+    } else {
+        contentDiv.innerHTML = "<p>Access Denied</p>";
+        console.error("Access denied:", data.error);
+    }
+}
+
+function initializePage() {
+    const input = document.getElementById('input-box');
+    const token = localStorage.getItem("token");
+
+    if (token === null || token.length === 0) {
+        console.log("token not found, request input")
+        input.addEventListener('keydown', (event) => {
+            if (event.key === 'Enter') {
+                login(input.value);
+            }
+        })
+    } else {
+        console.log("attempting data fetch since token already exists");
+        fetchData();
+    }
+}
+
+addEventListener("DOMContentLoaded", initializePage);
--- js/main.mjs
+++ js/main.mjs
@@ -1,6 +1,5 @@
 function initializePage() {
     generateFooter();
-    // applyTheming();
 }
 
 function getRandomInt(max) {
@@ -12,25 +11,6 @@ function generateFooter() {
     const currYear = new Date().getFullYear();
     const instanceText = "INSTANCE #" + getRandomInt(10000);
     document.querySelector('.footer-flair-label').textContent = currYear + " // " + instanceText + " // " + nineDigitsFlavor;
-}
-
-function applyTheming() {
-    var now = new Date();
-    var hour = now.getHours();
-
-    console.log("Current Hour:", hour);
-
-    var body = document.querySelector(".tiled-background");
-
-    if (hour >= 9 && hour < 15) {
-        body.style.webkitFilter = "none"; 
-        body.style.filter = "none"; 
-        body.style.background = 'url("img/backgroundtiling.png") repeat';
-    } else {
-        body.style.webkitFilter = "invert(100%)"; 
-        body.style.filter = "invert(100%)"; 
-        body.style.background = 'url("img/backgroundtiling-dark.png") repeat';
-    }
 }
 
 addEventListener("DOMContentLoaded", initializePage);
